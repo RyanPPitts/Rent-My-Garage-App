8287a1d8b4a96d87e925ccc7a964aba8eaa348fc
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-02-26 20:18:16 -0800

    File upload capabilty
    Creating file upload capability.

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import Dropzone from 'react-dropzone';
+import { Icon } from 'antd';
 
 function FileUpload() {
-  return <div>File Upload</div>;
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <Dropzone onDrop multiple maxSize>
+        {({ getRootProps, getInputProps }) => (
+          <div
+            style={{
+              width: '300px',
+              height: '240px',
+              border: '1px solid black',
+              display: 'flex',
+              alignItems: 'center'
+            }}
+            {...getRootProps()}
+          >
+            <input {...getInputProps()} />
+            <Icon type="plus" style={{ fontSize: '3rem' }} />
+          </div>
+        )}
+      </Dropzone>
+    </div>
+  );
 }
 
 export default FileUpload;
