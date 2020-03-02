10dcd6bb0592b194209991ad9dec15308ba5cd33
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-03-01 19:19:59 -0800

    Update file upload.

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Dropzone from 'react-dropzone';
 import { Icon } from 'antd';
 import Axios from 'axios';
-
 function FileUpload(props) {
   const [Images, setImages] = useState([]);
 
@@ -12,28 +11,36 @@ function FileUpload(props) {
       header: { 'content-type': 'multipart/form-data' }
     };
     formData.append('file', files[0]);
-
+    //save the Image we chose inside the Node Server
     Axios.post('/api/product/uploadImage', formData, config).then(response => {
       if (response.data.success) {
         setImages([...Images, response.data.image]);
         props.refreshFunction([...Images, response.data.image]);
       } else {
-        alert('Failed to save image in server');
+        alert('Failed to save the Image in Server');
       }
     });
+  };
 
-    // save the image we choose inside the node server
+  const onDelete = image => {
+    const currentIndex = Images.indexOf(image);
+
+    let newImages = [...Images];
+    newImages.splice(currentIndex, 1);
+
+    setImages(newImages);
+    props.refreshFunction(newImages);
   };
 
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-      <Dropzone onDrop={onDrop} multiple={false} maxSize={80000000000}>
+      <Dropzone onDrop={onDrop} multiple={false} maxSize={800000000}>
         {({ getRootProps, getInputProps }) => (
           <div
             style={{
               width: '300px',
               height: '240px',
-              border: '1px solid lightblue',
+              border: '1px solid lightgray',
               display: 'flex',
               alignItems: 'center',
               justifyContent: 'center'
@@ -54,9 +61,15 @@ function FileUpload(props) {
           overflowX: 'scroll'
         }}
       >
-        <div onClick>
-          <img />
-        </div>
+        {Images.map((image, index) => (
+          <div onClick={() => onDelete(image)}>
+            <img
+              style={{ minWidth: '300px', width: '300px', height: '240px' }}
+              src={`http://localhost:5000/${image}`}
+              alt={`productImg-${index}`}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
