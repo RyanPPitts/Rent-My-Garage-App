fcaafecddf216ba0a592c007b41d62427056afb7
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-03-11 19:10:44 -0700

    Creating CheckBox

---
+++ client/src/components/views/LandingPage/Sections/CheckBox.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const locations = [
+  {
+    "_id': 1,
+    name: 'Washington'
+  },
+  {
+    _id: 2,
+    name: 'Oregon'
+  },
+  {
+    _id: 3,
+    name: 'Idaho'
+  },
+  {
+    _id: 4,
+    name: 'California'
+  },
+  {
+    _id: 5,
+    name: 'Other'
+  }
+];
+
+function CheckBox() {
+  return <div></div>;
+}
+
+export default CheckBox;
