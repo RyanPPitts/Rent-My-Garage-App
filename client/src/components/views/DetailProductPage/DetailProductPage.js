4c0213da09d093a00b9b9da276c3ba2f0fdaacdf
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-03-16 20:51:55 -0700

    AddToCart Page

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -3,8 +3,9 @@ import Axios from 'axios';
 import { Row, Col } from 'antd';
 import ProductImage from './Sections/ProductImage';
 import ProductInfo from './Sections/ProductInfo';
-// import { addToCart } from '../../../_actions/user_actions';
+import { addToCart } from '../../../_actions/user_actions';
 import { useDispatch } from 'react-redux';
+
 function DetailProductPage(props) {
   const dispatch = useDispatch();
   const productId = props.match.params.productId;
@@ -18,9 +19,9 @@ function DetailProductPage(props) {
     );
   }, []);
 
-  // const addToCartHandler = productId => {
-  //   dispatch(addToCart(productId));
-  // };
+  const addToCartHandler = productId => {
+    dispatch(addToCart(productId));
+  };
 
   return (
     <div className="postPage" style={{ width: '100%', padding: '3rem 4rem' }}>
@@ -35,7 +36,7 @@ function DetailProductPage(props) {
           <ProductImage detail={Product} />
         </Col>
         <Col lg={12} xs={24}>
-          {/* <ProductInfo addToCart={addToCartHandler} detail={Product} /> */}
+          <ProductInfo addToCart={addToCartHandler} detail={Product} />
         </Col>
       </Row>
     </div>
